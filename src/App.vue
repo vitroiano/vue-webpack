5c1e2ae3259dc91d9c2d69ce35f6bf9edda88293
Vinicius Trainotti <vtrainotti@live.com>
2020-03-12 15:59:54 -0300

    ROUTING SETUP

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <div id="app">
-    <app-message></app-message>
+    <h1>Let's go somewhere!</h1>
+    <hr>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 // import HelloWorld from "./components/HelloWorld.vue";
-import Message from "./components/Message.vue";
+// import Message from "./components/Message.vue";
 
 export default {
-  name: "App",
-  components: {
-    'app-message': Message
-  }
+  name: "App"
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from "vue";
+import VueRouter from "vue-router";
 import App from "./App.vue";
+import Users from "./components/Users.vue";
+import Home from "./components/Home.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  { path: "/users", component: Users},
+  { path: "/", component: Home },
+];
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+});
 
 Vue.config.productionTip = false;
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount("#app");
