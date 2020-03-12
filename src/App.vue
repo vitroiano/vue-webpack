6b157fdbb0e7600a47fee038376842aee2ba374c
Vinicius Trainotti <vtrainotti@live.com>
2020-03-12 15:08:36 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <app-message></app-message>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from "./components/HelloWorld.vue";
+import Message from "./components/Message.vue";
+
+export default {
+  name: "App",
+  components: {
+    'app-message': Message
+  }
+};
+</script>
+
+<style>
+/* #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>
